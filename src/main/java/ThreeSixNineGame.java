55accdde1f10b7a64e512c3733485482b27ae1c6
jzero2 <jzero2@gmail.com>
2019-12-11 13:54:56 +0900

    12 로직 정리

--- src/main/java/ThreeSixNineGame.java
+++ src/main/java/ThreeSixNineGame.java
@@ -1,17 +1,10 @@
 public class ThreeSixNineGame {
     public String claps(int i) {
-        String result = "";
-        int remainder = (i > 10)? i % 10: i;
-        if (remainder > 0 && remainder % 3 == 0) {
-            result = "O";
-        } else {
-            result = "X" ;
-        }
-
+        int remainder = i % 10;
+        String result = (remainder > 0 && remainder % 3 == 0)? "O" : "X";
         if (i/10 > 0) {
             result = claps(i/10) + result;
         }
-
         return result;
     }
 }
